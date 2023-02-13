11e4224f8343272d2677293077afb9000b1413f2
uttam kumar patra <dev.uttampatra@gmail.com>
2023-02-13 14:12:55 +0530

    day 3 new

--- index.js
+++ index.js
@@ -6,3 +6,43 @@ const scrollHeader = () =>{
                        : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+/*=============== SERVICES MODAL ===============*/
+const modelViews = document.querySelectorAll('.services__model'),
+      modelBtn = document.querySelectorAll('.services__button'),
+      modelClose = document.querySelectorAll('.services__model-close');
+
+const model = (modalClick) =>{
+    modelViews[modalClick].classList.add('active__model');
+}
+modelBtn.forEach((mb, i)=>{
+    mb.addEventListener('click', () =>{
+        model(i);
+    });
+});
+
+modelClose.forEach((i)=>{
+    console.log(i);
+    i.addEventListener('click', () =>{
+        modelViews.forEach((mv)=>{
+            mv.classList.remove('active__model');
+        });
+    }); 
+});
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+
+
+/* Link active work */ 
+
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== LIGHT DARK THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
